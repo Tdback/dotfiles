9de9fc342522817612f97f97a44ee2162711f4ac
Tyler <tylerdback.etc@proton.me>
2023-08-15 17:08:33 -0400

    Made netrw more minimal for quick browsing.

--- .config/nvim/lua/td/init.lua
+++ .config/nvim/lua/td/init.lua
@@ -31,3 +31,8 @@ autocmd({"BufRead","BufNewFile"}, {
     pattern = "*.md",
     command = "setlocal spell",
 })
+
+-- minimal netrw
+vim.g.netrw_browse_split = 0
+vim.g.netrw_banner = 0
+vim.g.netrw_winsize = 25
