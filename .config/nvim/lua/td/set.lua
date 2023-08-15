2e77335c6fe535b626dc7936f60ef59c36fb942d
Tyler <tylerdback.etc@proton.me>
2023-08-15 12:46:45 -0400

    Slight rework in set.lua

--- .config/nvim/lua/td/set.lua
+++ .config/nvim/lua/td/set.lua
@@ -1,9 +1,10 @@
 vim.opt.guicursor = ""
 vim.opt.mouse = "v"
 vim.opt.mouse = "a"
+vim.opt.showcmd = true
 
-vim.opt.nu = true 
-vim.opt.relativenumber = true 
+vim.opt.nu = true
+vim.opt.relativenumber = true
 
 vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
@@ -11,9 +12,8 @@ vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
 
 vim.opt.smartindent = true
-
 vim.opt.wrap = false
- 
+
 vim.opt.swapfile = false
 vim.opt.backup = false
 vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
@@ -23,13 +23,19 @@ vim.opt.hlsearch = false
 vim.opt.incsearch = true
 
 vim.opt.termguicolors = true
+vim.opt.list = true
+vim.opt.lcs:append({ tab = "→\\ ", trail = '·', nbsp = '·' })
 
 vim.opt.scrolloff = 8
 vim.opt.signcolumn = "yes"
 vim.opt.isfname:append("@-@")
 
+vim.opt.foldmethod = "syntax"
+vim.opt.foldlevel = 99
+
 vim.opt.updatetime = 50
 
 vim.opt.colorcolumn = "100"
-vim.opt.cursorline = true 
+vim.opt.cursorline = true
 vim.opt.cursorlineopt = "number"
+
