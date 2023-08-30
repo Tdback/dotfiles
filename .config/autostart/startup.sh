0a87c7a15cfddb6f21df7346ab3a1a79c54e80db
Tyler <tylerdback.etc@proton.me>
2023-08-30 14:00:21 -0400

    Added comment header.

--- .config/autostart/startup.sh
+++ .config/autostart/startup.sh
@@ -1,5 +1,13 @@
 #!/usr/bin/env sh
 
+################################################################################
+# @author      : Tyler Dunneback (tylerdback@pm.me)
+# @file        : startup.sh
+# @created     : Wed Aug 30 01:59:27 PM EDT 2023
+#
+# @description : Commands to run on bspwm startup.
+#################################################################################
+
 # Set key-repetition rate
 xset r rate 350 40
 
